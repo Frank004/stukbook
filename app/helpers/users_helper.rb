5c9ff1b046cfad3f3dec81febd6e4c91155fe963
francisco Quinones <frank004@gmail.com>
2015-09-10 10:32:53 -0400

    Added Friendship status detection

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,14 @@
 module UsersHelper
+	def action_buttons(user)
+		case current_user.friendship_status(user) 
+		when "friends"
+				"Remove Friends"
+		when "pending"
+				"Cancel Request"		
+		when "requested"
+				"Accep or Deny"
+		when "not_friends"
+				"Add as a Friend"		
+	    end
+	end		
 end
