23a695d18f13240978525230fe7f251188e3b105
francisco Quinones <frank004@gmail.com>
2015-09-10 21:16:41 -0400

    Friendships actions now possible

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   root "pages#home"
   devise_for :users, controllers: {registrations: 'registrations'}
   resources :users, only: [:show, :index]
+  resources :friendships, only: [:create, :destroy, :accept] do
+      member do
+        put :accept
+      end
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
